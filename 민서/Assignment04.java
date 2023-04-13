8a6748be0ea88dda3034a80e4729d98c62a7322a
박민서 <qkrals2475@naver.com>
2023-04-14 01:16:35 +0900

    백엔드 2주차 과제

--- 민서/Assignment04.java
+++ 민서/Assignment04.java
@@ -5,11 +5,12 @@ import java.util.stream.Collectors;
 
 
 
-class SS{
+class SS{   // 클래스 생성
     private String name;
     private int level;
     private int power;
 
+    //  private 변수들을 반환하기 위해 접근
     public String getName() {
         return name;
     }
@@ -22,42 +23,40 @@ class SS{
         return power;
     }
 
-    SS(String name, int level, int power){
+    SS(String name, int level, int power){      // 매개변수로 3개의 변수를 받는 생성자
         this.name =name;
         this.level = level;
         this.power = power;
     }
 
-    int showPower(SS a, SS b){
-       return this.power + a.power + b.power;
-    }
 }
 public class Assignment04 {
     public static void main(String[] args) {
-    List<SS> ss = new ArrayList<>(Arrays.asList(
+    List<SS> ss = new ArrayList<>(Arrays.asList(    //List 인터페이스의 SS 클래스타입 ss라는 인스턴스를 만들어서 생성자 3개 만듬
       new SS ("James",10,3000),
             new SS("Alice",20,4000),
             new SS("Thomas",15,3500)
     ));
 
-
-
-    List<String> aaa=
+     List<String> aaa=
             ss.stream()
-                    .map(SS::getName)
-                    .collect(Collectors.toList());
-       System.out.println(aaa);
+                    .map(SS::getName) //SS클래스 타입의 ss 인스턴스를 String 타입으로 바꾸어 getName메서드를 호출
+                    .collect(Collectors.toList()); //ss스트림의 모든 요소들을 컬렉션에 저장
+        System.out.println("[user name]");
+       System.out.println(aaa+"\n"); // aaa출력
 
      int powersum = ss.stream()
-             .mapToInt(SS::getPower)
-             .sum();
-        System.out.println(powersum);
+             .mapToInt(SS::getPower)    // ss스트림의 요소들을 SS클래스의 getPower메서드의 조건에 맞게 int형으로 변환
+             .sum(); // getPower메서드로 반환받은 값을 다 더함
+        System.out.println("[user power 합계]");
+        System.out.println(powersum+"\n"); //powersum 출력
+
 
     List<String> name12 = ss.stream()
-            .filter(user -> user.getLevel()>=15)
-            .map(SS::getName)
-            .sorted()
-            .collect(Collectors.toList());
+            .filter(p -> p.getLevel()>=15)  //getLevel로 반환받은 level변수의 값이 15보다 크거나 같은 값만 필터링함
+            .map(SS::getName)  // ss스트림의 요소들을 SS클래스의 getName메서드의 조건에 맞게 String형으로 변환
+            .sorted()// 스트림 요소를 정렬 기본으로 오름차순 정렬
+            .collect(Collectors.toList());// 스트림의 모든 요소들을 컬렉션에 저장
         System.out.println("[user level 15 이상]");
         System.out.println(name12);
     }
